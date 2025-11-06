0b34cd70d00177cf3f85536c858df4a1894a9fa3
zzulaykhabinti <zzulaykhabinti@tudelft.nl>
2025-11-06 21:46:04 +0100

    Removed the output images i accidently pushed into the github

--- sine_wave.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def create_sine_wave(frequency,duration):
-    t = np.linspace(0, duration, int(44100 * duration))
-    return np.sin(2 * np.pi * frequency * t)
-
-print(create_sine_wave(440,2))
